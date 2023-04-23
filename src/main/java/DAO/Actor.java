3fca355a3082b59b1d5246d0513486f3961d591f
jihad mimouni <jihadmimouni6@gmail.com>
2023-04-23 21:16:50 +0100

    adding delete to actor and producer

--- src/main/java/DAO/Actor.java
+++ src/main/java/DAO/Actor.java
@@ -1,18 +1,52 @@
 package DAO;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import movie4u.models.Actors;
+
 public class Actor {
-	public static boolean login(String username,String password) throws SQLException {
+	public static boolean check(String username, String password) throws SQLException {
 		Connection con = Cnx.getInstance();
 		java.sql.Statement cstmt = con.createStatement();
-		ResultSet rs=cstmt.executeQuery("select movie4u.check_actor('"+username+"','"+password+"') from dual");
-		if (rs.getInt(1)!=1) 
+		ResultSet rs = cstmt.executeQuery("select movie4u.check_actor('" + username + "','" + password + "') from dual");
+		if (rs.getInt(1) != 1)
 			return false;
-		Cnx.setUSER(username);
-		Cnx.setPASS(password);
 		return true;
+	}
+
+	public static Actors get(String username, String password) throws SQLException, IOException {
+		Connection con = Cnx.getInstance();
+		java.sql.Statement cstmt = con.createStatement();
+		ResultSet rs = cstmt.executeQuery("select movie4u.check_actor( '"+username+"' , '"+password+"' ) from dual");
+		int k = 0;
+		ResultSet ls, ks;
+		Actors user=null;
+
+		while (rs.next()) {
+			k = rs.getInt(1);
+			if (k == 1) {
+				cstmt = con.createStatement();
+				ls = cstmt.executeQuery("select movie4u.get_actor_log( '"+username+"' , '"+password+"' ) from dual");
+				ls.next();
+				ks = (ResultSet) ls.getObject(1);
+				while (ks.next()) {
+					user=new Actors(ks.getString("NAME"),ks.getString("EMAIL"),ks.getString("PASSWORD"),ks.getDate("BIRTHDATE"),image.get_image(ks.getInt("IMAGE_ID")));
+					user.setID(ks.getInt("ID"));
+				}
+
+			}
+
+		}
+		return user;
+	}
+	public static void delete(String name) throws SQLException {
+		Connection con = Cnx.getInstance();
+		java.sql.Statement cstmt = con.createStatement();
+		cstmt.executeQuery("select movie4u.DELETE_actor('"+name+"') from dual");
+		System.exit(0);
+		
 	}
 }
--- src/main/java/DAO/Producer.java
+++ src/main/java/DAO/Producer.java
@@ -1,18 +1,52 @@
 package DAO;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import movie4u.models.Producers;
+
 public class Producer {
-	public static boolean login(String username,String password) throws SQLException {
+	public static boolean check(String username, String password) throws SQLException {
 		Connection con = Cnx.getInstance();
 		java.sql.Statement cstmt = con.createStatement();
-		ResultSet rs=cstmt.executeQuery("select movie4u.check_producer('"+username+"','"+password+"') from dual");
-		if (rs.getInt(1)!=1) 
+		ResultSet rs = cstmt.executeQuery("select movie4u.check_producer('" + username + "','" + password + "') from dual");
+		if (rs.getInt(1) != 1)
 			return false;
-		Cnx.setUSER(username);
-		Cnx.setPASS(password);
 		return true;
+	}
+
+	public static Producers get(String username, String password) throws SQLException, IOException {
+		Connection con = Cnx.getInstance();
+		java.sql.Statement cstmt = con.createStatement();
+		ResultSet rs = cstmt.executeQuery("select movie4u.check_producer( '"+username+"' , '"+password+"' ) from dual");
+		int k = 0;
+		ResultSet ls, ks;
+		Producers user=null;
+
+		while (rs.next()) {
+			k = rs.getInt(1);
+			if (k == 1) {
+				cstmt = con.createStatement();
+				ls = cstmt.executeQuery("select movie4u.get_producer_log( '"+username+"' , '"+password+"' ) from dual");
+				ls.next();
+				ks = (ResultSet) ls.getObject(1);
+				while (ks.next()) {
+					user=new Producers(ks.getString("NAME"),ks.getString("EMAIL"),ks.getString("PASSWORD"),ks.getDate("BIRTHDATE"),image.get_image(ks.getInt("IMAGE_ID")));
+					user.setID(ks.getInt("ID"));
+				}
+
+			}
+
+		}
+		return user;
+	}
+	public static void delete(String name) throws SQLException {
+		Connection con = Cnx.getInstance();
+		java.sql.Statement cstmt = con.createStatement();
+		cstmt.executeQuery("select movie4u.DELETE_producer('"+name+"') from dual");
+		System.exit(0);
+		
 	}
 }
