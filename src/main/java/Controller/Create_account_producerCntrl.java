631c68d2051d811231ba07f18dfa638d9027a8ce
21627 <21627@LAPTOP-I5LB6072>
2023-04-24 00:35:26 +0100

    javafx

--- src/main/java/Controller/Create_account_producerCntrl.java
+++ src/main/java/Controller/Create_account_producerCntrl.java
@@ -1,7 +1,9 @@
 package Controller;
 
+import java.io.File;
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,6 +11,7 @@ import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 import Connect.CnxDB;
+import DAO.Producer;
 //import Connect.DAOProducer;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -24,6 +27,8 @@ import javafx.scene.control.TextField;
 import movie4u.models.Producers;
 
 public class Create_account_producerCntrl implements Initializable {
+	  @FXML
+	    private TextField path_image;
 	 @FXML
 	    private Label L1;
 	@FXML
@@ -62,39 +67,33 @@ public class Create_account_producerCntrl implements Initializable {
     	
     	if((count>0) && (length>=8)) {
     	 
-    		//System.out.println("pssstt");
+    		 
     	int s = 0;
     	LocalDate d1=date.getValue();
     	String nom=name.getText();
     	String Email=email.getText();
     	String date=d1.toString();
         String mot_de_passe=password.getText();
+        File f=new File(path_image.getText());
+        Date d=Date.valueOf(d1);
+        
+        Producers p=new Producers(nom,Email,mot_de_passe,d,f);
+        Producer.insert(p);
     	
-    //*	Producers e=new Producers();
-    /*	e.setName(nom);
-    	e.setEmail(Email);
-    	e.setBirthdate(d1);
-    	e.setPassword(mot_de_passe);
-    	 
     	
-    	s=DAOProducer.save(e);*/
     	if(s>0) {
     		Alert al=new Alert(AlertType.INFORMATION);
     		al.setTitle("add producer !!");
     		al.setHeaderText("Information !");
-    		al.setContentText("BIEN AJOUTE !!!!!");
+    		al.setContentText("Well Added !!!!!");
     		al.showAndWait();
-    		/*i.clear();
-    		n.clear();
-        	p.clear();
-        	d.setValue(null);
-        	niv.setValue(null);*/
+    		 
     		
     	}else {
     		Alert al=new Alert(AlertType.ERROR);
     		al.setTitle("add producer !!");
     		al.setHeaderText("Information !");
-    		al.setContentText("NON  AJOUTE !!!!!");
+    		al.setContentText("NOT Added !!!!!");
     		al.showAndWait();
     		
     	}
