d69c361ccd2c21b3a8a1c868e8b3c33b3c497091
jihad mimouni <jihadmimouni6@gmail.com>
2023-04-23 20:26:21 +0100

    fixing DATE attribute

--- src/main/java/movie4u/models/Producers.java
+++ src/main/java/movie4u/models/Producers.java
@@ -1,11 +1,12 @@
 package movie4u.models;
 
 import java.io.File;
-import java.time.LocalDate;
+import java.sql.Date;
+
 
 public class Producers extends Users {
 
-	  public Producers(String name,String Email,String password,LocalDate birthdate,File image) {
+	  public Producers(String name,String Email,String password,Date birthdate,File image) {
 		  super(name,Email,password,birthdate,image);
 	  }
 
