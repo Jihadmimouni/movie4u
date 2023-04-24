004652f73fe5cce1606241457af97f6f26fc7bd8
jihad mimouni <jihadmimouni6@gmail.com>
2023-04-24 13:04:47 +0100

    adding some functions

---
+++ src/main/java/DAO/Synopsis.java
@@ -0,0 +1,17 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Synopsis {
+	public static movie4u.models.Synopsis get(int id) throws SQLException {
+		 Connection con = Cnx.getInstance();
+		java.sql.Statement cstmt = con.createStatement();
+		ResultSet rs = cstmt.executeQuery("select movie4u.get_synopsis('"+id+"') from dual");
+		rs = (ResultSet) rs.getObject(1);
+		return new movie4u.models.Synopsis(video.get(rs.getInt("VIDEO_ID")), rs.getString("TEXT"), rs.getBoolean("isText"));
+		}
+
+	}
+}
---
+++ src/main/java/DAO/video.java
@@ -0,0 +1,17 @@
+package DAO;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class video {
+	public static File get(int id) throws SQLException, IOException {
+		 Connection con = Cnx.getInstance();
+		 java.sql.Statement cstmt = con.createStatement();
+		 ResultSet rs = cstmt.executeQuery("select movie4u.get_video('"+id+"') from dual");
+		 rs = (ResultSet) rs.getObject(1);
+		 return Convert.toFile(rs.getBlob("video"));
+	}
+}
