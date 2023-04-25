4baea19a7a316ccdccd2765250e7bb75125f0138
jihad mimouni <jihadmimouni6@gmail.com>
2023-04-25 19:08:12 +0100

    adding rating and comments

--- src/main/java/DAO/Admin.java
+++ src/main/java/DAO/Admin.java
@@ -3,6 +3,11 @@ package DAO;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import movie4u.models.Comments;
+import movie4u.models.Rating;
 
 public class Admin {
  public static boolean check(int id) throws SQLException {
@@ -14,6 +19,26 @@ public class Admin {
 			return false;
 		return true;
  }
- 
+ public static Rating get_rate(int media_id) throws SQLException {
+	 Connection con = Cnx.getInstance();
+	 java.sql.Statement cstmt = con.createStatement();
+	 ResultSet rs = cstmt.executeQuery("select movie4u.get_average_rating('"+media_id+"') from dual");
+	 rs.next();
+	 return new Rating(rs.getInt("SCORE"));
+}
+	public static Comments get_comments(int media_id) throws SQLException {
+		Connection con = Cnx.getInstance();
+		java.sql.Statement cstmt = con.createStatement();
+		List<String> l=new ArrayList<String>();
+		ResultSet rs = cstmt.executeQuery("select movie4u.get_comments_by_media_id( '"+media_id+"' ) from dual");
+		rs.next();
+		rs = (ResultSet) rs.getObject(1);
+		while (rs.next())
+			l.add(rs.getString("COMMENTS"));
+		cstmt = con.createStatement();
+		rs = cstmt.executeQuery("select movie4u.get_media_name_id ('"+media_id+"' ) from dual");
+		rs.next();
+		return new Comments(l,rs.getString("NAME"));
+	} 
  
 }
--- src/main/java/DAO/Producer.java
+++ src/main/java/DAO/Producer.java
@@ -5,8 +5,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import movie4u.models.Comments;
 import movie4u.models.Producers;
+import movie4u.models.Rating;
 
 public class Producer {
 	public static boolean check(String username, String password) throws SQLException {
@@ -88,6 +92,25 @@ public class Producer {
 			System.out.println("error here");
 		}
 	}	
-	
-	
+	public static Rating get_rate(int media_id) throws SQLException {
+		 Connection con = Cnx.getInstance();
+		 java.sql.Statement cstmt = con.createStatement();
+		 ResultSet rs = cstmt.executeQuery("select movie4u.get_average_rating('"+media_id+"') from dual");
+		 rs.next();
+		 return new Rating(rs.getInt("SCORE"));
+	}
+	public static Comments get_comments(int media_id) throws SQLException {
+		Connection con = Cnx.getInstance();
+		java.sql.Statement cstmt = con.createStatement();
+		List<String> l=new ArrayList<String>();
+		ResultSet rs = cstmt.executeQuery("select movie4u.get_comments_by_media_id( '"+media_id+"' ) from dual");
+		rs.next();
+		rs = (ResultSet) rs.getObject(1);
+		while (rs.next())
+			l.add(rs.getString("COMMENTS"));
+		cstmt = con.createStatement();
+		rs = cstmt.executeQuery("select movie4u.get_media_name_id ('"+media_id+"' ) from dual");
+		rs.next();
+		return new Comments(l,rs.getString("NAME"));
+	} 
 }
