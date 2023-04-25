e4f6549bbfce807766e15d0c80c04d33b57c6774
jihad mimouni <jihadmimouni6@gmail.com>
2023-04-25 16:34:28 +0100

    adding search by actor

--- src/main/java/DAO/Media.java
+++ src/main/java/DAO/Media.java
@@ -44,7 +44,39 @@ public class Media {
 		return l;
 	}
 	
-	
+	public static List<movie4u.models.Media> get_actor_name(String name) throws SQLException, IOException{
+		List<movie4u.models.Media> l= new ArrayList<movie4u.models.Media>();
+		Connection con = Cnx.getInstance();
+		java.sql.Statement cstmt = con.createStatement();
+		ResultSet ks = cstmt.executeQuery("select movie4u.get_media_actor('"+name+"') from dual");
+		ks = (ResultSet) ks.getObject(1);
+		
+		while (ks.next()){
+			cstmt = con.createStatement();
+			ResultSet rs = cstmt.executeQuery("select movie4u.get_media_id('"+ks.getInt("MEDIA_ID")+"') from dual");
+			rs = (ResultSet) ks.getObject(1);
+		while (rs.next()) {
+			if (rs.getString("TYPE")=="SERIE") {
+				cstmt = con.createStatement();
+				ResultSet ls = cstmt.executeQuery("select movie4u.get_serie('"+rs.getInt("ID")+"') from dual");
+				ls =(ResultSet) ls.getObject(1);
+				l.add(Serie.convert(ls));
+			}
+			else if (rs.getString("TYPE")=="FILM") {
+				cstmt = con.createStatement();
+				ResultSet ls = cstmt.executeQuery("select movie4u.get_FILM('"+rs.getInt("id")+"') from dual");
+				ls =(ResultSet) ls.getObject(1);
+				l.add(Film.convert(ls));
+			}
+			else {
+				continue;
+			}
+		}
+		
+	}
+		return l;
+	}
+
 	
 	
 	
