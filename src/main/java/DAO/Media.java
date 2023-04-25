9ce7a4432357997223a93c10189f25080541da4f
jihad mimouni <jihadmimouni6@gmail.com>
2023-04-25 16:38:02 +0100

    adding search by producer

--- src/main/java/DAO/Media.java
+++ src/main/java/DAO/Media.java
@@ -77,7 +77,33 @@ public class Media {
 		return l;
 	}
 
-	
+	public static List<movie4u.models.Media> get_byproducer_name(String name) throws SQLException, IOException{
+		List<movie4u.models.Media> l= new ArrayList<movie4u.models.Media>();
+		Connection con = Cnx.getInstance();
+		java.sql.Statement cstmt = con.createStatement();
+		ResultSet rs = cstmt.executeQuery("select movie4u.get_media_producer('"+name+"') from dual");
+		rs = (ResultSet) rs.getObject(1);
+		while (rs.next()) {
+			if (rs.getString("TYPE")=="SERIE") {
+				cstmt = con.createStatement();
+				ResultSet ls = cstmt.executeQuery("select movie4u.get_serie('"+rs.getInt("id")+"') from dual");
+				ls =(ResultSet) ls.getObject(1);
+				l.add(Serie.convert(ls));
+			}
+			else if (rs.getString("TYPE")=="FILM") {
+				cstmt = con.createStatement();
+				ResultSet ls = cstmt.executeQuery("select movie4u.get_FILM('"+rs.getInt("id")+"') from dual");
+				ls =(ResultSet) ls.getObject(1);
+				l.add(Film.convert(ls));
+			}
+			else {
+				continue;
+			}
+		}
+		
+		
+		return l;
+	}
 	
 	
 	
