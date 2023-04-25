ff990ab833955de08e58c2ab26e44020e2a7f071
RahmaKhlifi <rahma.khlifi08@gmail.com>
2023-04-25 18:22:30 +0100

    Update user home profile and SearchView

--- src/main/java/Controller/SearchViewCntrl.java
+++ src/main/java/Controller/SearchViewCntrl.java
@@ -1,15 +1,20 @@
 package Controller;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.TextField;
+import movie4u.models.Media;
 
-public class SearchViewCntrl {
+public class SearchViewCntrl implements Initializable {
 
     @FXML
     private TextField searchActor;
@@ -22,80 +27,99 @@ public class SearchViewCntrl {
 
     @FXML
     private TextField searchname;
+    
+  
 
     @FXML
-    void SearchbyActor(ActionEvent event) throws IOException {
+    void SearchbyActor(ActionEvent event) throws IOException, SQLException {
     	FXMLLoader loader = new FXMLLoader(getClass().getResource("/Vue/ResultView.fxml"));
     	Parent root = loader.load();
+    	 ObservableList<Media> MediaList ;
+    	 MediaList =  (ObservableList<Media>) DAO.Media.get_BYactor_name(searchActor.getText().toString());
     	
     	ResultViewCntrl rvc = new ResultViewCntrl();
     	try {
-			rvc.DisplayINFO_film(searchActor.getText().toString());
+			rvc.DisplayINFO_film( MediaList);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
     	
     	try {
-			rvc.DisplayINFO_Serie(searchActor.getText().toString());
+			rvc.DisplayINFO_Serie(MediaList);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    @FXML
+    void SearchbyName(ActionEvent event) throws IOException, SQLException {
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/Vue/ResultView.fxml"));
+    	Parent root = loader.load();
+    	 ObservableList<Media> MediaList ;
+    	 MediaList =  (ObservableList<Media>) DAO.Media.get_byname(searchname.getText().toString());
+    	
+    	ResultViewCntrl rvc = new ResultViewCntrl();
+    	try {
+			rvc.DisplayINFO_film( MediaList);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    	
+    	try {
+			rvc.DisplayINFO_Serie(MediaList);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
     }
 
     @FXML
-    void SearchbyGenre(ActionEvent event) throws IOException {
+    void SearchbyGenre(ActionEvent event) throws IOException, SQLException {
     	FXMLLoader loader = new FXMLLoader(getClass().getResource("/Vue/ResultView.fxml"));
     	Parent root = loader.load();
+    	 ObservableList<Media> MediaList ;
+    	 MediaList =  (ObservableList<Media>) DAO.Media.get_bygenre(searchname.getText().toString());
     	
-    	ResultViewCntrl rvc = new ResultViewCntrl();
+    	ResultViewCntrl rvc = loader.getController();
     	try {
-			rvc.DisplayINFO_film(searchGenre.getText().toString());
+			rvc.DisplayINFO_film( MediaList);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+    	
     	try {
-			rvc.DisplayINFO_Serie(searchGenre.getText().toString());
+			rvc.DisplayINFO_Serie(MediaList);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
     }
 
+   
+
     @FXML
-    void SearchbyName(ActionEvent event) throws IOException {
+    void SearchbyProducer(ActionEvent event) throws IOException, SQLException {
     	FXMLLoader loader = new FXMLLoader(getClass().getResource("/Vue/ResultView.fxml"));
     	Parent root = loader.load();
+    	 ObservableList<Media> MediaList ;
+    	 MediaList =  (ObservableList<Media>) DAO.Media.get_byproducer_name(searchProducer.getText().toString());
     	
     	ResultViewCntrl rvc = new ResultViewCntrl();
     	try {
-			rvc.DisplayINFO_film(searchname.getText().toString());
+			rvc.DisplayINFO_film( MediaList);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
     	
     	try {
-			rvc.DisplayINFO_Serie(searchname.getText().toString());
+			rvc.DisplayINFO_Serie(MediaList);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
     }
 
-    @FXML
-    void SearchbyProducer(ActionEvent event) throws IOException {
-    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/Vue/ResultView.fxml"));
-    	Parent root = loader.load();
-           
-    	ResultViewCntrl rvc = new ResultViewCntrl();
-    	try {
-			rvc.DisplayINFO_Serie(searchProducer.getText().toString());
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-    	
-    	try {
-			rvc.DisplayINFO_Serie(searchProducer.getText().toString());
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-    }
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/main/java/Controller/User_profileCntrl.java
+++ src/main/java/Controller/User_profileCntrl.java
@@ -3,14 +3,18 @@ package Controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
+import movie4u.models.Media;
 import movie4u.models.Users;
 
 public class User_profileCntrl implements Initializable {
@@ -18,7 +22,7 @@ public class User_profileCntrl implements Initializable {
 	//public static Users profile ;
 
     @FXML
-    private TableColumn<?, ?> BdateColomn;
+    private TableColumn<Users, String> BdateColomn;
 
     @FXML
     private DatePicker birthTxt;
@@ -27,7 +31,7 @@ public class User_profileCntrl implements Initializable {
     private Button editBtn;
 
     @FXML
-    private TableColumn<?, ?> emailColomn;
+    private TableColumn<Users, String> emailColomn;
 
     @FXML
     private TextField emailTxt;
@@ -36,16 +40,22 @@ public class User_profileCntrl implements Initializable {
     private ImageView image;
 
     @FXML
-    private TableColumn<?, ?> nameColomn;
+    private TableColumn<Users, String> nameColomn;
 
     @FXML
     private TextField nameTxt;
 
     @FXML
-    private TableColumn<?, ?> pswrdColomn;
+    private TableColumn<Users, String> pswrdColomn;
 
     @FXML
     private TextField pswrdTxt;
+    
+    @FXML
+    private TableView<Users> tableinfo;
+
+    
+
 
     @FXML
     void Edit(ActionEvent event) {
@@ -60,12 +70,20 @@ public class User_profileCntrl implements Initializable {
 	}
 	
 	
-	void displayINFO(Users profile) {
+	void displayINFO( ObservableList<Users> Userinfo) {
 		 
-		nameColomn.setText(profile.getName());
-		emailColomn.setText(profile.getEmail());
-		pswrdColomn.setText(profile.getPassword());
-		BdateColomn.setText(profile.getBirthdate().toString());
+		   Userinfo.stream()
+		           .forEach(info ->{
+		        	   nameColomn.setCellValueFactory(new PropertyValueFactory<>("name"));
+		        	   emailColomn.setCellValueFactory(new PropertyValueFactory<>("email"));
+		        	   pswrdColomn.setCellValueFactory(new PropertyValueFactory<>("password"));
+		        	   BdateColomn.setCellValueFactory(new PropertyValueFactory<>("Birthdate"));
+           	            tableinfo.getItems().add(info);   
+		           });
+		           
+		        		   
+		        		   
+		        		   
 		
 	}
 	
